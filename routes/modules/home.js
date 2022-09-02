57de1e7aea9601965ecf6a613cecefb5e5f67593
Kris <krischiang0301@gmail.com>
2022-09-02 15:40:26 +0800

    feat: Add sort and search feature in index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,11 +3,33 @@ const routes = express.Router()
 const Record = require('../../models/Record')
 
 routes.get('/', (req, res) => {
+	const sortList = ['amount', '-amount', 'category', 'time']
+	const sortOption = sortList.includes(req.query.sort) ? req.query.sort : 'time'
 	const userId = req.user._id
 	Record.find({ userId })
 		.lean()
+		.sort(sortOption)
 		.then((record) => {
 			res.render('index', { record })
+		})
+		.catch((err) => console.log(err))
+})
+
+routes.get('/search', (req, res) => {
+	const keyword = req.query.keyword
+	if (!keyword) {
+		res.redirect('/')
+	}
+	const keywordLowerCase = keyword.trim().toLowerCase()
+	Record.find()
+		.lean()
+		.then((record) => {
+			const filterRecord = record.filter(
+				(item) =>
+					item.name.trim().toLowerCase().includes(keywordLowerCase) ||
+					item.name.includes(keywordLowerCase)
+			)
+			res.render('index', { record: filterRecord, keyword })
 		})
 		.catch((err) => console.log(err))
 })
