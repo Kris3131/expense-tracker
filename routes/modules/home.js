9a75674787b2d8fd3840ce7b9e7b60471a0b7b36
Kris <krischiang0301@gmail.com>
2022-09-04 10:24:32 +0800

    fix: categoryFilter

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,46 +4,42 @@ const Record = require('../../models/Record')
 const Category = require('../../models/Category')
 
 routes.get('/', (req, res, next) => {
-	const sortList = ['amount', '-amount', 'category', 'time']
-	const sortOption = sortList.includes(req.query.sort) ? req.query.sort : 'time'
 	const userId = req.user._id
-	let icon = ''
+	const filterCategory = req.query.filterCategory
+	console.log(filterCategory)
 	let totalAmount = 0
-	Record.find({ userId })
-		.lean()
-		.sort(sortOption)
-		.then((record) => {
-			record.forEach((item) => {
-				totalAmount = totalAmount + item.amount
-				item.date = item.date.toLocaleDateString()
-				Category.findById(item.categoryId)
-					.then((category) => {
-						return (icon = category.icon)
-					})
-					.catch((err) => next(err))
+	if (!filterCategory) {
+		Record.find({ userId })
+			.populate('categoryId')
+			.sort({ _id: 'asc' })
+			.lean()
+			.then((record) => {
+				record.forEach((item) => {
+					totalAmount = totalAmount + item.amount
+					item.date = item.date.toLocaleDateString()
+					Category.findById(item.categoryId)
+						.then((category) => {
+							return (icon = category.icon)
+						})
+						.catch((err) => next(err))
+				})
+				return res.render('index', { record, totalAmount })
 			})
-			return res.render('index', { record, totalAmount, icon })
+			.catch((err) => next(err))
+	} else {
+		Category.findOne({ name: filterCategory }).then((category) => {
+			const categoryId = category._id
+			Record.find({ categoryId })
+				.lean()
+				.then((item) => {
+					console.log(item)
+					totalAmount = totalAmount + item.amount
+					item.date = item.date.toLocaleDateString()
+				})
+				.catch((err) => next(err))
+			// return res.render('index', { item, totalAmount })
 		})
-		.catch((err) => next(err))
-})
-
-routes.get('/search', (req, res, next) => {
-	const keyword = req.query.keyword
-	if (!keyword) {
-		res.redirect('/')
 	}
-	const keywordLowerCase = keyword.trim().toLowerCase()
-	Record.find()
-		.lean()
-		.then((record) => {
-			const filterRecord = record.filter(
-				(item) =>
-					item.name.trim().toLowerCase().includes(keywordLowerCase) ||
-					item.name.includes(keywordLowerCase)
-			)
-			res.render('index', { record: filterRecord, keyword })
-		})
-		.catch((err) => next(err))
 })
 
 module.exports = routes
